f76dbb4f5ed2d6a99ba19c818e27e763a4c098dd
Dana-Harb <danaharb24@gmail.com>
2024-03-03 16:02:05 +0200

    Complete sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@
 import {Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import AuthPage from './pages/AuthPage';
+import PageLayout from './Components/PageLayout'
 function App() {
   
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path='/auth' element={<AuthPage/>}/>
-    </Routes>
-    </>
+    <PageLayout>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/auth' element={<AuthPage/>}/>
+      </Routes>
+    </PageLayout>
   );
 }
 
