ee9c9d10eff68daa475d9f51a89065167ebd0877
Dana-Harb <danaharb24@gmail.com>
2024-06-19 19:51:49 +0300

    Fetch Profile Header Data

--- src/Components/Profile/ProfileHeader.jsx
+++ src/Components/Profile/ProfileHeader.jsx
@@ -1,34 +1,46 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import useUserProfileStore from "../../store/useUserProfileStore"
+import useAuthStore from './../../store/authStore';
 
 const ProfileHeader = () => {
+  const {userProfile} = useUserProfileStore();
+  const authUser = useAuthStore(state=>state.user);
+  const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+  const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:'column',sm:"row"}}>
 
       <AvatarGroup size={{base:'xl',md:'2xl'}} justifySelf={'center'} alignSelf={'flex-start'} mx={'auto'}>
-        <Avatar alt='xodana' name='xodana' src='/profileLogo.png'/>
+        <Avatar alt='xodana' src={userProfile.profilePicURL}/>
       </AvatarGroup>
 
       <VStack alignItems={'start'} gap={2} mx={'auto'} flex={1}>
         {/*First Item in stack */}
         <Flex gap={4} direction={{base:'column',sm:'row'}} justifyContent={{base:'center',sm:'flex-start'}} alignItems={'center'} w={'full'}>
-          <Text fontSize={{base:'sm',md:'lg'}}> xodana </Text>
-          <Flex gap={4} alignItems={'center'} justifyContent={'center'}>
-            <Button bg={'gray.700'} color={'white'} _hover={{bg:'gray.800'}} size={{base:'xs',md:'s'}} p={2}>Edit Profile</Button>
-          </Flex>  
+          <Text fontSize={{base:'sm',md:'lg'}}> {userProfile.username} </Text>
+          { visitingOwnProfileAndAuth && 
+          (<Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+            <Button bg={'white'} color={'black'} _hover={{bg:'whiteAlpha.800'}} size={{base:'xs',md:'s'}} p={2}>Edit Profile</Button>
+          </Flex>)}   
+          {visitingAnotherProfileAndAuth && 
+          (<Flex gap={4} alignItems={'center'} justifyContent={'center'}>
+            <Button bg={'blue.500'} color={'white'} _hover={{bg:'blue.600'}} size={{base:'xs',md:'s'}} p={2}>Follow</Button>
+          </Flex>)}  
         </Flex>
         {/*Second Item in stack */}
         <Flex alignItems={'center'} gap={{base:2,sm:4}}>
-          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>4</Text>Posts</Text>
-          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>149</Text>Followers</Text>
-          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>254</Text>Following</Text>
+          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>{userProfile.posts.length}</Text>Posts</Text>
+          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>{userProfile.followers.length}</Text>Followers</Text>
+          <Text fontSize={{base:'xs',md:'sm'}}><Text as='span' fontWeight={'bold'} mr={1}>{userProfile.following.length}</Text>Following</Text>
         </Flex>
         {/*Third Item in stack */}
         <Flex alignItems={'center'} gap={4}>
-          <Text fontSize={'sm'} fontWeight={'bold'}>XoDana</Text>
+          <Text fontSize={'sm'} fontWeight={'bold'}>{userProfile.fullName}</Text>
         </Flex>
         {/*Fourth Item in stack */}
         <Flex alignItems={'center'} gap={4}>
-          <Text fontSize={'sm'}>Handmade Accesories made with love.</Text>
+          <Text fontSize={'sm'}>{userProfile.bio}</Text>
         </Flex>
       </VStack> 
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,11 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-
- 
 const firebaseConfig = {
   apiKey: "AIzaSyAAVNYn0BK53w2_hCJhg__5vexAVhrM0E4",
   authDomain: "insta-clone-c0b29.firebaseapp.com",
@@ -15,10 +11,11 @@ const firebaseConfig = {
   appId: "1:621132320483:web:1f5f6de678f1c218f3ffd6",
   measurementId: "G-HEFLSRXFDV"
 };
-
 const app = initializeApp(firebaseConfig);
 const auth= getAuth(app);
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth , firestore, storage };
+
+
